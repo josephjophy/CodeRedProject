7c8922deb8cbd709e89b0629764c37db67f356c8
Joseph Jophy <josephjophy@Josephs-MacBook-Air-2.local>
2022-11-03 20:46:16 -0500

    Added a Procfile.

---
+++ Desktop/login/dataentryform.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2> Animal Entry Form </h2>
+<form action="/action_page.php" name="Animal_Data_Entry">
+    <!--Create text boxes for each entry of data-->
+    <label for="Animal_Name"> Name </label>
+    <input type="text" id="Animal_Name" name="Animal_Name"><br>
+
+    <label for="Species"> Species </label>
+    <input type="text" id="Species" name="Species"><br>
+
+    <label for="Animal_Age"> Animal Age </label>
+    <input type="text" id="Animal_Age" name="Animal_Age"><br>
+
+    <label for="Diet"> Diet </label>
+    <input type="text" id="Diet" name="Diet"><br>
+
+    <!--Submit button to save/submit all entries-->
+    <input type="submit" value="Submit"><br>
+
+</body>
+</html>
